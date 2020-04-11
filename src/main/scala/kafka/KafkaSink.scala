d193aed53e07a5a825052f3d80a7fd94481b652f
vas1l3v <pvasilev9525@gmail.com>
2020-04-11 15:21:11 +0300

    Add streaming function for easy console debugging

--- src/main/scala/kafka/KafkaSink.scala
+++ src/main/scala/kafka/KafkaSink.scala
@@ -1,5 +1,24 @@
 package kafka
 
-class KafkaSink {
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.streaming.StreamingQuery
 
+class KafkaSink {
+  def loadStreamToHDFS(dfInputStream: DataFrame): StreamingQuery = {
+    dfInputStream
+      .writeStream
+      .queryName("Debug stream from kafka")
+      .format("delta")
+      .outputMode("append")
+      .option("checkpointLocation", "delta path")
+      .start("delta path")
+  }
+
+  def debugStream(dfInputStream: DataFrame): StreamingQuery = {
+    dfInputStream
+      .writeStream
+      .queryName("Debug stream from kafka")
+      .format("console")
+      .start()
+  }
 }
